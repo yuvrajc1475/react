df985c99a509346e66ff790c1989e9df1cda87e5
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-05-18 19:57:35 +0530

    currency coverter using custom hook

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
-import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import useCurrencyInfo from "../hooks/useCurrencyInfo";
 import { InputBox } from "./components/index.js";
 
 function App() {
