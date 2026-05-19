d57196ac2693df3b4170a9be53732c5ea1827b68
Yuvraj <yuvrajchopra9817239199@gmail.com>
2026-05-19 19:16:35 +0530

    react router setup

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='underline text-3xl'>working!</h1>
+  )
+}
+
+export default App
